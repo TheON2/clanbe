c414a6feb961817f556019c03804c67111c28d3e
TheON2 <sniperad@naver.com>
2024-03-28 21:06:49 +0900

    Fix : remove Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <p className="bg-slate-900 text-white py-2 text-sm text-center">
+    <p className="py-2 text-base text-center">
       {
-        "The pain is so persistent that it is like a snail, and the joy is so short that it is like a rabbit's tail running through the fields of autumn | All Right Reserved."
+        
       }
     </p>
   );
