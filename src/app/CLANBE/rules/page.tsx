22f4ae1a48dcc2b4456b8463980d12ba9dda8d29
TheON2 <sniperad@naver.com>
2024-04-13 15:48:22 +0900

    Update : rules component

--- src/app/CLANBE/rules/page.tsx
+++ src/app/CLANBE/rules/page.tsx
@@ -22,9 +22,9 @@ export default function RulesPage() {
           <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
             <Image
               alt="nextui logo"
-              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-              width={60}
-              height={60}
+              src="https://img.icons8.com/ios/100/swearing-male.png"
+              width={100}
+              height={100}
             />
           </div>
           <div className="relative pt-20 px-4 pb-4 w-full">
@@ -69,9 +69,9 @@ export default function RulesPage() {
           <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
             <Image
               alt="nextui logo"
-              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-              width={60}
-              height={60}
+              src="https://img.icons8.com/ios-filled/200/laptop-coding.png"
+              width={200}
+              height={200}
             />
           </div>
           <div className="relative pt-20 px-4 pb-4 w-full">
@@ -118,9 +118,9 @@ export default function RulesPage() {
           <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
             <Image
               alt="nextui logo"
-              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-              width={60}
-              height={60}
+              src="https://img.icons8.com/ios/100/sparring.png"
+              width={100}
+              height={100}
             />
           </div>
           <div className="relative pt-20 px-4 pb-4 w-full">
@@ -167,9 +167,9 @@ export default function RulesPage() {
           <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
             <Image
               alt="nextui logo"
-              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-              width={60}
-              height={60}
+              src="https://img.icons8.com/external-outline-geotatah/100/external-clan-estate-planning-outline-geotatah.png"
+              width={100}
+              height={100}
             />
           </div>
           <div className="relative pt-20 px-4 pb-4 w-full">
@@ -191,8 +191,104 @@ export default function RulesPage() {
           </div>
           <div className="m-2">
             <p className="text-1xl">2항</p>
+            <p className="text-lg">적발시 제명</p>
+          </div>
+        </CardBody>
+      </Card>
+      <Card
+        className={`min-w-[350px] max-w-[400px] mt-24 overflow-visible w-1/3 ${styles.customCard}`}
+      >
+        <CardHeader className="relative p-0">
+          <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
+            <Image
+              alt="nextui logo"
+              src="https://img.icons8.com/ios-filled/100/twitch.png"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="relative pt-20 px-4 pb-4 w-full">
+            <p className="text-4xl font-bold block text-center">
+              방송경기 치팅
+            </p>
+            <p className="text-xl text-gray-500 block text-center">
+              방송경기 중 부정행위
+            </p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody className="p-4 text-center min-h-[320px]">
+          <div className="m-2">
+            <p className="text-1xl">1항</p>
+            <p className="text-lg">
+              방송경기 참여중 방플 / 귀맵을
+              <br />
+              일체 금합니다.
+            </p>
+          </div>
+          <div className="m-2">
+            <p className="text-1xl">2항</p>
+            <p className="text-lg">
+              방송경기 참여자에게
+              <br />
+              경기정보 전달 및 대리게임을
+              <br />
+              일체 금합니다.
+            </p>
+          </div>
+          <div className="m-2">
+            <p className="text-1xl">3항</p>
             <p className="text-lg">
               적발시
+              <br />
+              경기 몰수패 및 경고
+            </p>
+          </div>
+        </CardBody>
+      </Card>
+      <Card
+        className={`min-w-[350px] max-w-[400px] mt-24 overflow-visible w-1/3 ${styles.customCard}`}
+      >
+        <CardHeader className="relative p-0">
+          <div className="w-36 h-36 rounded-full bg-blue-500 absolute -top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
+            <Image
+              alt="nextui logo"
+              src="https://img.icons8.com/ios-filled/100/box-important--v1.png"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="relative pt-20 px-4 pb-4 w-full">
+            <p className="text-4xl font-bold block text-center">경고</p>
+            <p className="text-xl text-gray-500 block text-center">
+              규정에 의거한 모든 경고
+            </p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody className="p-4 text-center min-h-[320px]">
+          <div className="m-2">
+            <p className="text-1xl">1항</p>
+            <p className="text-lg">
+              규정위반시 모든 경고는
+              <br />
+              통합되어 처리된다.
+              <br />
+              2경고 발생사안시 2경고 지급
+            </p>
+          </div>
+          <div className="m-2">
+            <p className="text-1xl">2항</p>
+            <p className="text-lg">
+              모든 경고는 부과일 기준
+              <br />
+              6개월 이후 삭제
+            </p>
+          </div>
+          <div className="m-2">
+            <p className="text-1xl">3항</p>
+            <p className="text-lg">
+              경고3회 누적시
               <br />
               제명
             </p>
