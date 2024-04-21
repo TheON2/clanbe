915ec1bb21c44034015dd3182e6a9e3d9f4f8070
TheON2 <laufetv@gmail.com>
2024-04-21 10:44:21 +0900

    Fix : tapnav

--- src/components/TabNav.tsx
+++ src/components/TabNav.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { getNavData } from "@/service/user";
+import { getNavData, getTeamData } from "@/service/user";
 import TapNavComponent from "./TapNavComponent";
 import TapNavComponent2 from "./TapNavComponent2";
 import { useSelector } from "react-redux";
@@ -8,18 +8,15 @@ import { TeamState } from "../../redux/reducers/teamSlice";
 import { useSession } from "next-auth/react";
 
 async function TapNav() {
-  const navData = await getNavData();
+  const { teams } = await getTeamData();
   const { data: session, status } = useSession(); // 세션 데이터와 상태 가져오기
   const isLoggedIn = status === "authenticated";
-
-  const user = useSelector((state: { user: UserState }) => state.user);
-  const teams = useSelector((state: { team: TeamState[] }) => state.team);
 
   return (
     <>
       {isLoggedIn ? <p>{session.user?.name}</p> : <p>로그인안됨</p>}
-      {user.nickname ? (
-        <TapNavComponent navData={{ user, teams }} />
+      {isLoggedIn ? (
+        <TapNavComponent user={session.user} teams={teams} />
       ) : (
         <TapNavComponent2 />
       )}
