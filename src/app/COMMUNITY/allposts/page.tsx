df02ce5ae85c1414b2008c32ba5f74d3d23ef6b1
TheON2 <sniperad@naver.com>
2024-04-23 13:41:16 +0900

    Update : posts router apply tag revalidate

--- src/app/COMMUNITY/allposts/page.tsx
+++ src/app/COMMUNITY/allposts/page.tsx
@@ -3,9 +3,23 @@ import { announce, board } from "../../../../public/data";
 import { getAllPosts } from "@/service/posts";
 
 export default async function AllPostPage() {
-  const posts = await getAllPosts();
+  // API 호출을 통해 포스트 데이터를 가져옴
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    next: { tags: ["post"] },
+  });
+
+  // 응답을 JSON으로 변환
+  const posts = await response.json();
   console.log(posts);
   return (
-    <BoardLayout boardTitle={"전체 게시글"} announce={posts} posts={posts} />
+    <BoardLayout
+      boardTitle={"전체 게시글"}
+      announce={posts.data}
+      posts={posts.data}
+    />
   );
 }
