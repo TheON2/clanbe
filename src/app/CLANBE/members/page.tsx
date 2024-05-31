4965b88f0abd7f2f33107ef7f7a49b8b80f96291
dowon kim <32028454+TheON2@users.noreply.github.com>
2024-05-31 12:34:59 +0900

    Delete src/app/CLANBE directory

--- src/app/CLANBE/members/page.tsx
+++
@@ -1,8 +0,0 @@
-import MemberComponent from "@/components/MemberComponent";
-import { getUsers } from "@/service/user";
-
-
-export default async function MembersPage() {
-  const users = await getUsers();
-  return <MemberComponent users={users.users} />;
-}
--- src/app/CLANBE/schedule/page.tsx
+++
@@ -1,7 +0,0 @@
-import ScheduleComponent from "@/components/ScheduleComponent";
-import { getEvent } from "@/service/schedule";
-
-export default async function SchedulePage() {
-  const schedule = await getEvent();
-  return <ScheduleComponent events={schedule} />;
-}
